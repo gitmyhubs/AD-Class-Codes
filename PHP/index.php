46b16b16efd8edfc0d6f1d384e753df951b4aa4c
Piyush <gitmyhubs@gmail.com>
2024-08-08 10:50:03 +0530

    PHP Introduction, Installatoin and First Program

---
+++ PHP/index.php
@@ -0,0 +1,26 @@
+<?php 
+//  to see PHP Version using Method
+//  echo phpversion();
+
+// To see information about PHP using this method
+//  echo phpinfo();
+
+
+// $msg="Welcome to Ad CLass";  
+// echo "Helloo: $msg"; 
+
+// Print multiline output
+// echo "Hello, Ad Class
+// Welcome To 
+// The Class,
+// And how are you!!  
+// ";  
+
+// Example of print
+// print "Hello Ad class by  print ";  
+// print ("Hello Ad Class by print()");  
+ ?>
+
+
+
+
