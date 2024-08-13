57801f82fadd9f6f1c0154990bfc265794fb8262
Piyush <gitmyhubs@gmail.com>
2024-08-13 16:12:37 +0530

    PHP Data Types

---
+++ PHP/php_datatypes.php
@@ -0,0 +1,34 @@
+<?php 
+// class Person {
+//     public $name;
+//     public $age;
+
+//     // Method to introduce the person
+//     public function introduce() {
+//         return "Hi, my name is {$this->name} and I am {$this->age} years old.";
+        
+//     }
+// }
+// // Create an object of the Person class
+// $person = new Person();
+// // Set properties
+// $person->name = "piyush";
+// $person->age = 100;
+
+// // Call the method
+// echo $person->introduce();
+
+
+?>
+
+<?php  
+$userDetails = ["Piyush", "piyush@gmail.com", "9900990099"];
+// var_dump($userDetails);
+echo "<br>";
+// echo $userDetails[2];
+echo "User Details is :" . $userDetails[0].  "<br>";
+echo "User Email is :" . $userDetails[1]. "<br>";
+echo "User Mobile is :" . $userDetails[2]. "<br>";
+
+// echo "user Details : " .$userDetails[0]. "<br>" . "Email : " .$userDetails[1]. "<br>" . "Mobile : " .$userDetails[2];
+?>
