19548c9143c27da3e5b3c5e02c415a701d3cc63c
Piyush <gitmyhubs@gmail.com>
2024-08-27 12:03:14 +0530

    PHP Control Statement

---
+++ PHP/php_control_statement.php
@@ -0,0 +1,99 @@
+<?php
+// Example of if conditon
+// $age = 20;
+// if ($age >= 34) {
+//     echo "App Adult Ho gaye hai.. ";
+// } else {
+//     echo "Abhi Aap Bache hai..";
+// }
+
+// example of ifelse condition
+// $temp = 30;
+// if ($temp > 40) {
+//     echo "It's a hot day.";
+// } else {
+//     echo "It's a cool day.";
+// }
+
+
+// Example of if-elseif-else
+// $stuMark = 65;
+// if ($stuMark >= 90) 
+// {
+//     echo "Grade: A";
+// } 
+// elseif ($stuMark >= 80) 
+// {
+//     echo "Grade: B";
+// } 
+// elseif ($stuMark >= 70) 
+// {
+//     echo "Grade: C";
+// } 
+// elseif ($stuMark >= 60) 
+// {
+//     echo "Grade: D";
+// } 
+// else 
+// {
+//     echo "No Grade... Failed";
+// }
+
+// Example of switch statement
+// $day = 'Tuesday';
+
+// switch ($day) {
+//     case 'Monday':
+//         echo "Today is Monday";
+//         break;
+//     case 'Tuesday':
+//         echo "Today is Tuesday";
+//         break;
+//     case 'Wednesday':
+//         echo "Today is Wednesday";
+//         break;
+//     case 'Thrusday':
+//         echo "Today is Thrusday";
+//         break;
+//     case 'Friday':
+//         echo "Today is Friday";
+//         break;
+//     case 'Saturday':
+//         echo "Today is Saturday";
+//         break;
+//     case 'Sunday':
+//         echo "Today is Sunday";
+//         break;
+//     default:
+//         echo "Another day.";
+//         break;
+// }
+
+// Example of for loop
+// for ($i = 1; $i < 10; $i++) {
+//     echo $i . " ";
+// }
+
+// Example of While loop
+// $num = 0;
+// while ($num < 10) {
+//     echo $num . " ";
+//     $num++;
+// }
+
+// Example of do-while loop
+// $num1 = 0;
+// do {
+//     echo "<h1 style='color:red; text-align:center;'>". $num1 . "</h1>";
+//     $num1++;
+// } 
+// while ($num1 < 20);
+
+// Example of foreach loop
+// $userDetails = ['Name', 'Email', 'Number', 'Address', 'Pincode'];
+// foreach ($userDetails as $user) {
+//     echo $user . " ";
+// }
+
+
+
